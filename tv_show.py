f1cb0a45d33081729559a4ea11f71baac165e815
Gabriel Wagner Piazenski <100728241+gabrielwp87@users.noreply.github.com>
2022-03-13 16:48:57 -0300

    Update tv_show.py
    made the inheritance better, by making the classes shorter

--- tv_show.py
+++ tv_show.py
@@ -20,26 +20,32 @@ class Programa:
     def nome(self, nome):
         self._nome = nome
 
-
+        
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
-        self._nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano) #usa os atributos da classe mãe
         self.duracao = duracao
-        self._likes = 0
+
 
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
-        self._nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano) #usa os atributos da classe mãe
         self.temporadas = temporadas
-        self._likes = 0
+
 
 
 
 vingadores = Filme("vingadores - guerra infinita", 2018, 160)
-print(vingadores.nome)
-
 atlanta = Serie('atlanta', 2018, 2)
-print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano}')
+
+vingadores.dar_likes()
+vingadores.dar_likes()
+vingadores.dar_likes()
+
+atlanta.dar_likes()
+atlanta.dar_likes()
+
+
+print(f'Nome: {vingadores.nome} = Likes: {vingadores.likes}')
+print(f'Nome: {atlanta.nome} - Likes: {atlanta.likes}')
